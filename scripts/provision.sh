83b2e56deec4c2176ab103ea790a251c31831d92
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-05 09:23:31 +0200

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -24,4 +24,4 @@ which jenkins &>/dev/null || {
 sudo systemctl enable jenkins.service
 sudo systemctl start jenkins.service
 
-echo "Unlock Jenkins password is:" && sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo echo "Unlock Jenkins password is:" && sudo cat /var/lib/jenkins/secrets/initialAdminPassword
