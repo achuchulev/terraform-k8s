bf17f4e24453a0455a915406fd2ab4e0da6665b9
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-05 10:52:10 +0200

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 # make sure apt database is up-to date
-sudo apt-get update
+sudo apt update
 
 # install tools
 which wget curl telnet unzip &>/dev/null || {
-  sudo apt-get install -y wget curl telnet unzip
+  sudo apt install -y wget curl telnet unzip
 }
 
 # install java-jdk required for jenkins to run
 which default-jdk &>/dev/null || {
-  sudo apt-get install -y default-jdk
+  sudo apt install -y default-jdk
 }
 
 # install jenkins  
 which jenkins &>/dev/null || {
   wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
   sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-  sudo apt-get update
-  sudo apt-get install -y jenkins
+  sudo apt update
+  sudo apt install -y jenkins
 }
 
 sudo systemctl enable jenkins.service
